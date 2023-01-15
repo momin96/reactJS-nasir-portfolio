f583d37b170366d82533c5d55f5b5331091d7065
Nasir <momin96@gmail.com>
2023-01-15 23:10:24 +0530

    Tech skills alignment completed

--- src/landing_components/TechSkills.js
+++ src/landing_components/TechSkills.js
@@ -7,14 +7,28 @@ const TechSkills = () => {
             <header id="skills-header">
                 Tech Skills
             </header>
-            <p>Platform: iOS, Android, Flutter, Web (ReactJS)</p>
-            <p>Languages: Swift, Kotlin, Java, Dart, Objective-C, JavaScript</p> 
-            <p>IDE: Xcode, Android Studio, VS Code</p>
-            <p>VCS: Github, GitLab, BitButcket</p>
-            <p>AI Tools: Github Copilot, ChatGPT, Dall-E</p>
-            <p></p>
+            <div className="container">
+                <p className="tech-skills-left">Platform: </p>
+                <p className="tech-skills-right">iOS, Android, Flutter, Web (ReactJS)</p>
+
+                <p className="tech-skills-left">Languages: </p>
+                <p className="tech-skills-right">Swift, Kotlin, Java, Dart, Objective-C, JavaScript</p>
+
+                <p className="tech-skills-left">VCS: </p>
+                <p className="tech-skills-right"> Github, GitLab, BitButcket</p>
+
+                <p className="tech-skills-left">Database: </p>
+                <p className="tech-skills-right"> Firebase RTDB, Firestore, CouchBase lite, CoreData SQLite, MySQL</p>
+
+                <p className="tech-skills-left">IDE: </p>
+                <p className="tech-skills-right"> Xcode, Android Studio, VS Code</p>
+
+                <p className="tech-skills-left">AI Tools: </p>
+                <p className="tech-skills-right"> Github Copilot, ChatGPT, Dall-E</p>
+            </div>
         </div>
     );
 }
+
 
 export default TechSkills;
