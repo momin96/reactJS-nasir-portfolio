27bd68bc7eec6323b706b3faeb0a45839661d3a5
Nasir <momin96@gmail.com>
2023-02-17 22:30:37 +0530

    Re arranged profile summary

--- src/landing_components/ProfileSummary.js
+++ src/landing_components/ProfileSummary.js
@@ -8,7 +8,10 @@ const ProfileSummary = () => {
                 NASIR AHMED MOMIN
             </header>
             <div id="experience">
-                <p className='experience-text'>I am a mobile app developer with 8+ years of experience in developing native iOS, Android and Flutter applications. I have expertise in developing high-quality, high-performance apps that are optimized for usability and user experience. I am proficient in Swift, Objective-C, Java, Kotlin and Dart languages. I can develop apps for both Android and iOS platforms, and can integrate them with various back-end services and APIs. I am confident that I can help you design and develop mobile apps tailored to your specific needs and requirements.</p>
+                <p className='experience-text'>I am a mobile app developer with 8+ years of experience in developing native iOS, Android and Flutter applications and can integrate them with various back-end services and APIs.</p>
+                <p className='experience-text'>I have expertise in developing high-quality, high-performance apps that are optimized for usability and user experience.</p>
+                <p className='experience-text'>I am proficient in Swift, Objective-C, Java, Kotlin and Dart languages.</p>
+                <p className='experience-text'>I can help you design and develop mobile apps tailored to your specific needs and requirements.</p>
             </div>
         </div>
     );
